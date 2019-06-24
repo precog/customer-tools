170efd2df18519523538fc91ca5661dfa06169c9
Daniel Sobral <dcsobral@gmail.com>
2019-06-24 14:18:44 -0600

    Reformat stringToJson

--- ETL/stringToJson.sh
+++ ETL/stringToJson.sh
@@ -1,4 +1,38 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
 
-TOTAL=100; i=1; j=0; k=all; NEXTTOKEN="null"; TABLE=projects; MAXITEMS=25; { DATA=$(aws dynamodb scan --output json --table-name $TABLE --max-items $MAXITEMS); ITEMS=$(echo $DATA | jq -r -c ".Items | .[]"); echo $ITEMS; NEXTTOKEN=$(echo $DATA | jq -r '.NextToken') && j=$MAXITEMS; echo $j >&2; while [ "${NEXTTOKEN}" != "null" ] && [ $j -lt $TOTAL ]; do DATA=$(aws dynamodb scan --output json --table-name $TABLE --starting-token $NEXTTOKEN --max-items $MAXITEMS); ITEMS=$(echo $DATA | jq -r -c ".Items | .[]"); echo $ITEMS; NEXTTOKEN=$(echo $DATA | jq -r '.NextToken'); j=$((j + MAXITEMS)); echo $j >&2; done; } | jq -r -c '"\(.)\n\(.)\n\(.)"' | while read -r line; do case $k in all) echo $line; k=pbd;; pbd) echo $line | jq -r -c '.projectBinaryData.B // "H4sIABWa/lwCA6uu5QIABrCh3QMAAAA="' | base64 --decode | gzip -d | jq -r -c '{ projectBinaryData: { B: . } }'; k=pd;; pd) echo $line | jq -r -c '.projectData.S // "{}" | fromjson | { projectData: { S: . } }'; k=all;; esac; done | paste - - - | jq -r -c '. + input + input'
-
+TOTAL=100
+i=1
+j=0
+k=all
+NEXTTOKEN="null"
+TABLE=projects
+MAXITEMS=25
+{
+	DATA=$(aws dynamodb scan --output json --table-name $TABLE --max-items $MAXITEMS)
+	ITEMS=$(echo "$DATA" | jq -r -c ".Items | .[]")
+	echo "$ITEMS"
+	NEXTTOKEN="$(echo "$DATA" | jq -r -c '.NextToken')" && j=$MAXITEMS
+	echo $j >&2
+	while [ "${NEXTTOKEN}" != "null" ] && [ $j -lt $TOTAL ]; do
+		DATA=$(aws dynamodb scan --output json --table-name $TABLE --starting-token "$NEXTTOKEN" --max-items $MAXITEMS)
+		ITEMS=$(echo "$DATA" | jq -r -c ".Items | .[]")
+		echo "$ITEMS"
+		NEXTTOKEN="$(echo "$DATA" | jq -r -c '.NextToken')"
+		j=$((j + MAXITEMS))
+		echo $j >&2
+	done
+} | jq -r -c '"\(.)\n\(.)\n\(.)"' | while read -r line; do case $k in all)
+	echo "$line"
+	k=pbd
+	;;
+pbd)
+	echo "$line" | jq -r -c '.projectBinaryData.B // "H4sIABWa/lwCA6uu5QIABrCh3QMAAAA="' | base64 --decode | gzip -d | jq -r -c '{ projectBinaryData: { B: . } }'
+	k=pd
+	;;
+pd)
+	echo "$line" | jq -r -c '.projectData.S // "{}" | fromjson | { projectData: { S: . } }'
+	k=all
+	;;
+esac done | paste - - - | jq -r -c '. + input + input'
