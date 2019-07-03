f552934c698e3b174b8f4ca4c4ef758e6d8b95a6
Daniel Sobral <dcsobral@gmail.com>
2019-07-03 11:59:46 -0600

    Reformated with shfmt
    It was close enough, and shfmt did catch some inconsistencies.

--- ETL/stringToJson.sh
+++ ETL/stringToJson.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if  [[ ! -x $(type -p jq) ]]; then
+if [[ ! -x $(type -p jq) ]]; then
 	cat >&2 <<-MISSING_JQ
 		Please install "jq". On Amazon AMI Linux, install it with:
-
+		
 		    sudo yum install -y jq
-
+		
 		See https://stedolan.github.io/jq/download/ for information on other
 		platforms.
 	MISSING_JQ
@@ -22,23 +22,23 @@ fi
 usage() {
 	cat >&2 <<-USAGE
 		Usage: $0 [-?|-h|--help] [<binary_path> <string_path>]
-
+		
 		-? | -h | --help             Prints this message
 		-a | --all                   Process all data (overrides max items)
 		-b N | --batch-size-items N  Process data in batches of N (defautls to 25)
 		-m N | --max-items N         Limits processing to the first N entries (defaults to 100)
 		-t NAME | --table NAME       DynamoDB table name (defaults to "projects")
-
+		
 		Paths are specified as .x.y.z for { "x": { "y": { "z": data }}}. More
 		generally, they must be valid "jq" paths.
-
+		
 		Binary paths must point to a string that contains a base64-encoded,
 		gzipped json, so that "base64 --decode | gzip -d" will turn that
 		string into valid json.
-
+		
 		String paths must point to a string that contains valid json. For
 		example, .x in { "x": "{ \"a\": 5 }" }.
-
+		
 		Use a non-existing path if there's no binary or string path. For
 		example, ".no.binary.path .path.to.string" if there's string data
 		on the .path.to.string, but not binary data, and ".no.binary.path"
@@ -49,12 +49,24 @@ usage() {
 
 while [[ $# -gt 0 && "$1" == -* ]]; do
 	case "$1" in
-		-\? | -h | --help) usage ;;
-		-a | --all) ALL=1 ;;
-		-b | --batch-size) shift; BATCH_SIZE="${1}" ;;
-		-m | --max-items) shift; MAX_ITEMS="${1}" ;;
-		-t | --table) shift; TABLE="${1}" ;;
-		*) echo >&2 "Invalid parameter '$1'"$'\n'; usage ;;
+	-\? | -h | --help) usage ;;
+	-a | --all) ALL=1 ;;
+	-b | --batch-size)
+		shift
+		BATCH_SIZE="${1}"
+		;;
+	-m | --max-items)
+		shift
+		MAX_ITEMS="${1}"
+		;;
+	-t | --table)
+		shift
+		TABLE="${1}"
+		;;
+	*)
+		echo >&2 "Invalid parameter '$1'"$'\n'
+		usage
+		;;
 	esac
 	shift
 done
@@ -89,19 +101,19 @@ NEXTTOKEN='null'
 j=0
 {
 	DATA=$(aws dynamodb scan --output json --table-name "$TABLE" --max-items "$BATCH_SIZE")
-	jq -r -c '.Items[]' <<< "$DATA"
-	NEXTTOKEN=$(jq -r '.NextToken' <<< "$DATA") && j="$BATCH_SIZE"
+	jq -r -c '.Items[]' <<<"$DATA"
+	NEXTTOKEN=$(jq -r '.NextToken' <<<"$DATA") && j="$BATCH_SIZE"
 	echo $j >&2
-	while [[ "${NEXTTOKEN}" != 'null' && ( -n "${ALL}" || $j -lt "$MAX_ITEMS" ) ]]; do
+	while [[ "${NEXTTOKEN}" != 'null' && (-n "${ALL}" || $j -lt "$MAX_ITEMS") ]]; do
 		DATA=$(aws dynamodb scan --output json --table-name "$TABLE" --starting-token "$NEXTTOKEN" --max-items "$BATCH_SIZE")
-		jq -r -c '.Items[]' <<< "$DATA"
-		NEXTTOKEN=$(jq -r '.NextToken' <<< "$DATA")
+		jq -r -c '.Items[]' <<<"$DATA"
+		NEXTTOKEN=$(jq -r '.NextToken' <<<"$DATA")
 		j=$((j + BATCH_SIZE))
 		echo $j >&2
 	done
 } | while read -r line; do
-    echo "$line" | jq -r -c "${BINARY_DEFAULT_QUERY}" | base64 --decode | gzip -d |
-	    jq --argjson line "$line" -r -c "${OUTPUT_QUERY}"
+	echo "$line" | jq -r -c "${BINARY_DEFAULT_QUERY}" | base64 --decode | gzip -d |
+		jq --argjson line "$line" -r -c "${OUTPUT_QUERY}"
 done
 
 # vim: set ts=4 sw=4 tw=100 noet :
