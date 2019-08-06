b9064e868d10b5040a3e9fa643e5c097f523ebbb
Daniel Sobral <dcsobral@slamdata.com>
2019-08-06 02:13:10 -0600

    Skeleton build for native app

---
+++ apps/dynamodb-etl/wrapper.h
@@ -0,0 +1,11 @@
+//
+// Created by Daniel Sobral on 2019-08-06.
+//
+
+#ifndef DYNAMODB_ETL_WRAPPER_H
+#define DYNAMODB_ETL_WRAPPER_H
+
+#include <jq.h>
+#include <jv.h>
+
+#endif //DYNAMODB_ETL_WRAPPER_H
